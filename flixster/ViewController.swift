f9fa07f25b76536e043eaf0214403e9ddb5f544a
Tenzin Rigchok <trigchok@buffalo.edu>
2021-09-12 11:48:00 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Tenzin Rigchok on 9/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
